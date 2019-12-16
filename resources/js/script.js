0a7cfe97538235d883a3b9a74b43944d3136c0ac
Nick Banken <Nick.Banken@gmail.com>
2019-12-16 16:41:46 +0100

    added 2 new pages to the website

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,45 +1,33 @@
-var image = document.getElementsByClassName('parallax__banner--1');
-let imageQ = document.querySelector('.parallax__banner--1');
-imageQ = imageQ.parentNode;
+function createParallax(imageN,classN,textP){
 
-let container = document.createElement('div');
-container.className = "parallax__wrapper"
-let text = document.createElement('h3')
-text.className = "parallax__text"
+    let image = document.getElementsByClassName(imageN);
+    let imageQ = document.querySelector(classN);
+    if(imageQ != null){
+    imageQ = imageQ.parentNode;
+   
+    let container = document.createElement('div');
+    container.className = "parallax__wrapper"
+    let text = document.createElement('h3')
+    text.className = "parallax__text"
 
-text.innerText = "Een knipbeurt op maat"
+    text.innerText = textP;
 
-container.append(text);
+    container.append(text);
 
-imageQ.append(container)
+    imageQ.append(container)
 
-var image2 = document.getElementsByClassName('parallax__banner--2');
-imageQ = document.querySelector('.parallax__banner--2');
-imageQ = imageQ.parentNode;
 
-container = document.createElement('div');
-container.className = "parallax__wrapper"
-text = document.createElement('h3')
-text.className = "parallax__text"
+    new simpleParallax(image, {
+        delay: 0,
+        orientation: 'down',
+        scale: 1.5,
+        overfow: true,
+    });
+    }
+}
 
-text.innerText = "Gezelligheid in bilzen"
+createParallax("parallax__banner--1",".parallax__banner--1","Een knipbeurt op maat" );
+createParallax("parallax__banner--2",".parallax__banner--2","Gezelligheid in bilzen" );
 
-container.append(text);
+createParallax("parallax__banner--3",".parallax__banner--3","Prijzen & services" );
 
-imageQ.append(container)
-
-console.log(imageQ);
-
-new simpleParallax(image, {
-    delay: 0,
-    orientation: 'down',
-    scale: 1.5,
-    overfow: true,
-});
-
-new simpleParallax(image2, {
-    delay: 0,
-    orientation: 'down',
-    scale: 1.5,
-    overfow: true,
-});
